b8295d153818e577007bd70097a34729a2952b9d
jonathan-martz <github@jmartz.de>
2019-12-12 21:38:21 +0100

    update depoyment config

--- .phan/config.php
+++ .phan/config.php
@@ -15,7 +15,9 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
-        'src/app/code/'
+        'src/app/code/',
+        'src/vendor/',
+        'src/generated/'
     ],
     // A directory list that defines files that will be excluded
     // from static analysis, but whose class and method
