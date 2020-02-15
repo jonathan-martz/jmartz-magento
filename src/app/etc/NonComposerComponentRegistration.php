5a56174f3ded6214e79ba6d5167b7d541b08b9e8
jonathan-martz <github@jmartz.de>
2020-02-15 18:21:53 +0100

    update gitignore, update composer

--- src/app/etc/NonComposerComponentRegistration.php
+++ src/app/etc/NonComposerComponentRegistration.php
@@ -15,19 +15,18 @@ use RuntimeException;
  * @throws RuntimeException
  * @return void
  */
-function main()
-{
+$main = function () {
     $globPatterns = require __DIR__ . '/registration_globlist.php';
     $baseDir = dirname(dirname(__DIR__)) . '/';
 
-    foreach ($globPatterns as $globPattern) {
+    foreach($globPatterns as $globPattern) {
         // Sorting is disabled intentionally for performance improvement
         $files = glob($baseDir . $globPattern, GLOB_NOSORT);
-        if ($files === false) {
+        if($files === false) {
             throw new RuntimeException("glob(): error with '$baseDir$globPattern'");
         }
         array_map(function ($file) { require_once $file; }, $files);
     }
-}
+};
 
-main();
+$main();
