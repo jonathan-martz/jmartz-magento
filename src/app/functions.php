f66fa4891dca30e008c402dff2b2bd4a89be2480
jonathan-martz <github@jmartz.de>
2020-02-17 21:45:14 +0100

    downgrade to 2.3.3

--- src/app/functions.php
+++ src/app/functions.php
@@ -4,29 +4,24 @@
  * See COPYING.txt for license details.
  */
 
-use Magento\Framework\Phrase;
-
 /**
  * Create value-object \Magento\Framework\Phrase
- * @return Phrase
- * @see        Magento\Framework\Phrase\__.php
- * @SuppressWarnings(PHPMD.ShortMethodName)
  * @deprecated The global function __() is now loaded via Magento Framework, the below require is only
  *             for backwards compatibility reasons and this file will be removed in a future version
+ * @see        Magento\Framework\Phrase\__.php
+ * @SuppressWarnings(PHPMD.ShortMethodName)
+ * @return \Magento\Framework\Phrase
  */
-if(!function_exists('__')) {
-    /**
-     * @return Phrase
-     */
+if (!function_exists('__')) {
     function __()
     {
         $argc = func_get_args();
 
         $text = array_shift($argc);
-        if(!empty($argc) && is_array($argc[0])) {
+        if (!empty($argc) && is_array($argc[0])) {
             $argc = $argc[0];
         }
 
-        return new Phrase($text, $argc);
+        return new \Magento\Framework\Phrase($text, $argc);
     }
 }
